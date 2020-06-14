129f3c00c5108ed2dd83e54c7515819fcd68dcc9
David Ralph <ohlookitsderpy@protonmail.com>
2020-06-14 13:22:31 +0100

    stuff

--- main.go
+++ main.go
@@ -20,27 +20,33 @@ func setupRouter() *gin.Engine {
 	})
 
 	router.POST("/upload", func(c *gin.Context) {
-		if c.Request.Header.Get("Authorization") != os.Getenv("SECRET") {
+		if c.Request.Header.Get("Authorization") != os.Getenv("SECRET") { // Make sure the token is correct
 			c.JSON(401, gin.H{"message": "Invalid token!"})
 			return
 		}
 		file, _ := c.FormFile("file")
-		filename := randomstring.New() + "." + strings.Split(file.Filename, ".")[1]
-		c.SaveUploadedFile(file, "./uploads/"+filename)
+		filename := randomstring.New() + "." + strings.Split(file.Filename, ".")[1] // Create random filename...
+		c.SaveUploadedFile(file, "./uploads/"+filename)                             // ...and move the file to ./uploads/
 		c.JSON(200, gin.H{"file": filename})
 	})
 
-	router.GET("/stats", func(c *gin.Context) {
+	router.GET("/stats", func(c *gin.Context) { // File count etc
 		files, _ := ioutil.ReadDir("./uploads")
 		c.JSON(200, gin.H{"fileCount": len(files)})
 	})
 
-	router.Use(static.Serve("/", static.LocalFile("./uploads", true)))
+	router.Use(static.Serve("/", static.LocalFile("./uploads", true))) // Static files in ./uploads/
 
 	return router
 }
 
 func main() {
+	// Create ./uploads if it doesn't already exist
+	if _, err := os.Stat("./uploads/"); os.IsNotExist(err) {
+		os.Mkdir("./uploads/", 777)
+	}
+
+	// Start webserver
 	router := setupRouter()
 	router.Run(os.Getenv("PORT"))
 }
